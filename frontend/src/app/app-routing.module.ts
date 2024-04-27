9a77c2c8ac4f91d84bf5c339fe00968abdf3617b
luccamarshall <52831210+luccamarshall@users.noreply.github.com>
2024-04-27 17:38:17 +0100

    router working

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AddPageComponent } from './add-page/add-page.component';
 import { WebsiteComponent } from './website/website.component';
 import { WebsiteViewComponent } from './website-view/website-view.component';
+import { WebsiteDetailsComponent } from './website-details/website-details.component';
 
 
 const routes: Routes = [
   {path: '', component: WebsiteComponent },
   {path: 'add-page', component: AddPageComponent},
-  {path: 'websites', component: WebsiteViewComponent}
+  {path: 'websites', component: WebsiteViewComponent},
+  {path: 'website-details', component: WebsiteDetailsComponent}
 ];
 
 @NgModule({
