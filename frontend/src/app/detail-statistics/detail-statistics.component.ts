f3e86caa066b6cdcca3a798b56103ba25f4fff90
fc57162 <fc57162@alunos.fc.ul.pt>
2024-05-23 17:40:16 +0100

    Filtros com erros

--- frontend/src/app/detail-statistics/detail-statistics.component.ts
+++ frontend/src/app/detail-statistics/detail-statistics.component.ts
@@ -10,26 +10,87 @@ import { IPage } from '../page';
   styleUrls: ['./detail-statistics.component.css']
 })
 export class DetailStatisticsComponent {
+  cwagFiltered: boolean = true;
+  actFiltered: boolean = true;
+
+  levelaaa: boolean = true;
+  levelaa: boolean = true;
+  levela: boolean = true;
+
+  passed: boolean = true;
+  failed: boolean = true;
+  warning: boolean = true;
+  inapplicable: boolean = false;
+
+  totalPassedTests: number = 0;
+  totalWarningTests: number = 0;
+  totalFailedTests: number = 0;
+  totalInapplicableTest: number = 0;
+
+  actRulesTestsResults: any[] = [];
+  wcagTestsResults: any[] = [];
+
+  filterList: any[] = []
+
+  website = {} as IWebsite;
+  page = {} as IPage;
+
   filterTests(s: string) {
     if(s === 'cwagFiltered'){
       this.cwagFiltered = !this.cwagFiltered;
     }
     else if (s === 'actFiltered'){
       this.actFiltered = !this.actFiltered;
+    } 
+    else if(s === 'passed'){
+      this.passed = !this.passed;
     }
-    console.log(this.actFiltered);
+    else if(s === 'failed'){
+      this.failed = !this.failed;
+    }
+    else if(s === 'warning'){
+      this.warning = !this.warning;
+    }
+    else if(s === 'not applicable'){
+      this.inapplicable = !this.inapplicable;
+    }
+    else if(s === 'levela'){
+      this.levela = !this.levela;
+    }
+    else if(s === 'levelaa'){
+      this.levelaa = !this.levelaa;
+    }
+    else if(s === 'levelaaa'){
+      this.levelaaa = !this.levelaaa;
+    }
   }
-  cwagFiltered: boolean = true;
-  actFiltered: boolean = true;
-  totalPassedTests: number = 0;
-  totalWarningTests: number = 0;
-  totalFailedTests: number = 0;
-  totalInapplicableTest: number = 0;
-  actRulesTestsResults: any[] = [];
-  wcagTestsResults: any[] = [];
-
-  website = {} as IWebsite;
-  page = {} as IPage;
+  filterRest(list: any[]){
+    let results: any[] = [];
+    for(let res of list){
+      if(!results.includes(res) && this.passed && res.outcome == "passed"){
+        results.push(res);
+      }
+      if(!results.includes(res) && this.failed && res.outcome == "failed"){
+        results.push(res);
+      }
+      if(!results.includes(res) && this.warning && res.outcome == "warning"){
+        results.push(res);
+      }
+      if(!results.includes(res) && this.inapplicable && res.outcome == "inapplicable"){
+        results.push(res);
+      }
+      if(this.levela && !results.includes(res) && res.successCriteria.includes("A")){
+        results.push(res);
+      }
+      else if(this.levelaa && !results.includes(res) && res.successCriteria.includes("AA")){
+        results.push(res);
+      }
+      else if(this.levelaaa && !results.includes(res) && res.successCriteria.includes("AAA")){
+        results.push(res);
+      }
+    }
+    return results;
+  }
 
 constructor(private route: ActivatedRoute, private websiteService: WebsiteService) {}
 
