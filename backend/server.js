688604a21ed31b877826c31819434c627a985e35
diogo freaza <fc56969@alunos.fc.ul.pt>
2024-05-24 12:06:38 +0100

    para funcionar localmente

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,8 @@ const { QualWeb, generateEARLReport } = require('@qualweb/core');
 
 const app = express();
 
-//mongoose.connect('mongodb://localhost:27017/myapp', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect('mongodb://psi021:psi021@localhost:27017/psi021?retryWrites=true&authSource=psi021');
+mongoose.connect('mongodb://localhost:27017/myapp', { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb://psi021:psi021@localhost:27017/psi021?retryWrites=true&authSource=psi021');
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -23,5 +23,5 @@ app.use('/', reportRoutes);
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
-//app.listen(3000, () => console.log('Server started on port 3000'));
-app.listen(3071, () => console.log('Server started on port 3071'));
+app.listen(3000, () => console.log('Server started on port 3000'));
+//app.listen(3071, () => console.log('Server started on port 3071'));
